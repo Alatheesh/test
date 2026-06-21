ba50fea705eb9d686aaa9b3856bed22d6eca3145
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2026-06-21 16:27:12 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    API_ID = int(os.environ.get("API_ID", 0))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    DB_URI = os.environ.get("DB_URI", "") # MongoDB Connection String
-    DB_NAME = os.environ.get("DB_NAME", "filter_bot")
